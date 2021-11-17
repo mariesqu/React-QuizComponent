1c9ee4270e510be3aafcb2aac23b1f277ed753e4
SNJ.MESQUIVEL <snj.mesquivel@cma-cgm.com>
2021-11-17 09:25:45 -0600

    Fixed Creating a QuizEnd Component

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,17 +5,19 @@ import QuizEnd from './QuizEnd'
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
-    render() {
-        const isQuizEnd = (this.state.quiz_position - 1) == quizData.quiz_questions.length;
-        return [
-            isQuizEnd ? <QuizEnd/> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
-        ]
-    }
-
     constructor(props) {
         super(props);
         this.state = {quiz_position: 1}
+    }
+
+    render() {
+        const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
+        return (
+            <div>
+                {isQuizEnd ? <QuizEnd/> :
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+            </div>
+        )
     }
 }
 
