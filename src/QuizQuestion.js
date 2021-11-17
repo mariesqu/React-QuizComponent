557d30ef832b3029bf65e667bb7e4653e314174a
SNJ.MESQUIVEL <snj.mesquivel@cma-cgm.com>
2021-11-17 09:12:19 -0600

    Creating a QuizQuestionButton Component

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import Quiz from "./Quiz";
+import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
     render() {
@@ -10,16 +10,11 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <li>{this.props.quiz_question.answer_options[0]}</li>
+                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
                     </ul>
                 </section>
             </main>
         )
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = {quiz_position: 1}
     }
 }
 
